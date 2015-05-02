d2fb73c41c8c747a5602eb72e959cacc7c4597fa
wwayne <wayneup@163.com>
2015-05-02 12:25:07 +0800

    test try

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,20 +1,18 @@
-// __tests__/index-test.js
-
 'use strict';
 
 jest.dontMock('../index');
 
 var React = require('react/addons');
-var ReactTooltip = require('../index');
-var TestUtils = React.addons.TestUtils;
+// var ReactTooltip = require('../index');
+// var TestUtils = React.addons.TestUtils;
 
-describe('react tooltip', function() {
-  var tooltip = TestUtils.renderIntoDocument(
-    <ReactTooltip />
-  );
-  it('should render data-placeholder', function() {
-    var span = TestUtils.findRenderDOMComponentWithTag(tooltip, "span");
-    expect(span.getDOMNode().textContent).toEqual("");
-
-  })
-})
+// describe('react tooltip', function() {
+//   var tooltip = TestUtils.renderIntoDocument(
+//     <ReactTooltip />
+//   );
+//   it('should render data-placeholder', function() {
+//     var span = TestUtils.findRenderDOMComponentWithTag(tooltip, "span");
+//     expect(span.getDOMNode().textContent).toEqual("");
+//
+//   })
+// })
