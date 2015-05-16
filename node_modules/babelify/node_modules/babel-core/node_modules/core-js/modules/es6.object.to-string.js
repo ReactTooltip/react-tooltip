61dededbc096a1f1c2f5ef8dc1bdddcf9edd837b
wwayne <wayneup@163.com>
2015-05-16 14:30:35 +0800

    progress

--- node_modules/babelify/node_modules/babel-core/node_modules/core-js/modules/es6.object.to-string.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// 19.1.3.6 Object.prototype.toString()
-var $   = require('./$')
-  , cof = require('./$.cof')
-  , tmp = {};
-tmp[require('./$.wks')('toStringTag')] = 'z';
-if($.FW && cof(tmp) != 'z')$.hide(Object.prototype, 'toString', function toString(){
-  return '[object ' + cof.classof(this) + ']';
-});
--- node_modules/react-tooltip/src/basic-show.js
+++
@@ -1,7 +0,0 @@
-var RCSS = require("rcss");
-
-var tooltipShow = {
-  opacity: '0.9 !important',
-  marginTop: '0px !important'
-}
-module.exports = RCSS.registerClass(tooltipShow);
