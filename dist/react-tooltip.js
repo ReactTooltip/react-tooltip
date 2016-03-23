d82832bc6a5df4745e149c9934ed61ad44163550
İlker Güller <me@ilkerguller.com>
2016-03-23 20:03:12 +0200

    Fixed CurrentItem Bug
    Fixed CurrentItem Bug

--- dist/react-tooltip.js
+++ dist/react-tooltip.js
@@ -167,7 +167,7 @@ var ReactTooltip = function (_Component) {
     value: function bindListener() {
       var targetArray = this.getTargetArray();
 
-      var dataEvent = undefined;
+      var dataEvent = void 0;
       for (var i = 0; i < targetArray.length; i++) {
         if (targetArray[i].getAttribute('currentItem') === null) {
           targetArray[i].setAttribute('currentItem', 'false');
@@ -194,7 +194,7 @@ var ReactTooltip = function (_Component) {
     key: 'unbindListener',
     value: function unbindListener() {
       var targetArray = document.querySelectorAll('[data-tip]');
-      var dataEvent = undefined;
+      var dataEvent = void 0;
 
       for (var i = 0; i < targetArray.length; i++) {
         dataEvent = this.state.event || targetArray[i].getAttribute('data-event');
@@ -217,7 +217,7 @@ var ReactTooltip = function (_Component) {
     value: function getTargetArray() {
       var id = this.props.id;
 
-      var targetArray = undefined;
+      var targetArray = void 0;
 
       if (id === undefined) {
         targetArray = document.querySelectorAll('[data-tip]:not([data-for])');
@@ -307,7 +307,7 @@ var ReactTooltip = function (_Component) {
       /* Detect multiline */
       var regexp = /<br\s*\/?>/;
       var multiline = e.currentTarget.getAttribute('data-multiline') ? e.currentTarget.getAttribute('data-multiline') : this.props.multiline ? this.props.multiline : false;
-      var tooltipText = undefined;
+      var tooltipText = void 0;
       var multilineCount = 0;
       if (!multiline || multiline === 'false' || !regexp.test(originTooltip)) {
         tooltipText = originTooltip;
@@ -390,9 +390,18 @@ var ReactTooltip = function (_Component) {
 
   }, {
     key: 'hideTooltip',
-    value: function hideTooltip() {
+    value: function hideTooltip(e) {
       var _this4 = this;
 
+      if (e && e.currentTarget) {
+        if (e.currentTarget.getAttribute('currentItem') === 'true') e.currentTarget.setAttribute('currentItem', 'false');
+      } else {
+        var items = Array.from(this.getTargetArray());
+        var currentItem = items.filter(function (item) {
+          return item.getAttribute('currentItem') === 'true';
+        });
+        if (currentItem[0]) currentItem[0].setAttribute('currentItem', 'false');
+      }
       var delayHide = this.state.delayHide;
 
       clearTimeout(this.delayShowLoop);
@@ -441,8 +450,8 @@ var ReactTooltip = function (_Component) {
       var targetHeight = currentTarget.clientHeight;
       var windoWidth = window.innerWidth;
       var windowHeight = window.innerHeight;
-      var x = undefined;
-      var y = undefined;
+      var x = void 0;
+      var y = void 0;
       var defaultTopY = targetTop - tipHeight - 8;
       var defaultBottomY = targetTop + targetHeight + 8;
       var defaultLeftX = targetLeft - tipWidth - 6;
@@ -714,8 +723,8 @@ var ReactTooltip = function (_Component) {
         }
         firstCount++;
       }
-      for (var i = string.length - 1; i >= 0; i--) {
-        if (string[i] !== ' ') {
+      for (var _i = string.length - 1; _i >= 0; _i--) {
+        if (string[_i] !== ' ') {
           break;
         }
         lastCount++;
