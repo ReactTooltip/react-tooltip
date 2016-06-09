f4f22e89e6e0cd316205ca01668b686a9348e5ef
Nick Earwood <nearwood@gmail.com>
2016-06-09 12:27:42 -0400

    Use absolute instead of fixed positioning.

--- bin/transferSass.js
+++ bin/transferSass.js
@@ -15,13 +15,14 @@ function transferSass () {
       if (err) {
         console.error(err)
       }
-      console.log('css file has been transformed successful')
+      console.log('css file has been transformed successfully')
     })
   })
 }
 
 transferSass()
 
+console.log('Watching scss file...')
 fs.watch(path.resolve(__dirname, '../src/index.scss'), function (event, filename) {
   console.log(event, filename)
   transferSass()
