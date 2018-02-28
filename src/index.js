0116f767e50808aa141e186b1200b5db86fef26a
Tomo Ishihara <tishihar94@users.noreply.github.com>
2018-02-28 13:38:08 -0800

    Fix issue where tooltip won't show for adjacent elements.

--- src/index.js
+++ src/index.js
@@ -234,7 +234,7 @@ class ReactTooltip extends Component {
       // Don't trigger other elements belongs to other ReactTooltip
       const targetArray = this.getTargetArray(this.props.id)
       const isMyElement = targetArray.some(ele => ele === e.currentTarget)
-      if (!isMyElement || this.state.show) return
+      if (!isMyElement) return
     }
     // Get the tooltip content
     // calculate in this phrase so that tip width height can be detected
