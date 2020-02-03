ad19add91d69a6f9ed48560a0456351510cca616
kebabmaster <ieshevyakov@gmail.com>
2020-02-03 21:22:22 +0000

    Tests - In Progress - Attempting Popup Colors CSS Checks

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,53 @@
+import React from 'react'
 import {expect} from 'chai'
-import '../src/index.js'
+const forEach = require('mocha-each')
+const jsdom = require('mocha-jsdom')
+import ReactTooltip from '../src/index.js'
+import { render, cleanup } from '@testing-library/react'
+import jss from 'jss'
+
+afterEach(cleanup)
 
 describe('Tooltip', () => {
-  it('COLOR GEN BASED ON PROPS // TODO', () => { // TODO: tests
-  	expect(2).to.equal(2)
-  })
+ 	jsdom({url: 'http://localhost/'})
+
+	forEach([[{'textColor': 'green', 'backgroundColor': 'red', 'arrowColor': 'blue'}]
+		]).it('Popup color generation', (props) => {
+
+
+
+//console.log(wrapper.renderer._element)
+
+
+// console.log(jss)
+
+	const tooltip = render(<ReactTooltip id='colorSpec' {...props}/>)
+		const style = window.getComputedStyle(document.getElementById('colorSpec'))
+
+
+		// console.log(document.getElementById('colorSpec'))
+		// console.dir(style)
+
+
+		// // console.dir(document.getElementById('colorSpec').classList[0])
+
+
+
+
+    var cssText = "";
+
+
+    var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+    for (var x = 0; x < classes.length; x++) {        
+        // if (classes[x].selectorText == className) {
+       		//console.dir(classes[x])
+       		       	//	console.dir(classes[x].selectorText)
+            cssText += classes[x].cssText || classes[x].style.cssText;
+        // }         
+    }
+    // console.log(cssText)
+
+
+		// //console.dir(window.getComputedStyle(tooltip))
+	})
 })
