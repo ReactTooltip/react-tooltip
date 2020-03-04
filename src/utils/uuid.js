b942fa4701ec5c4a845d15ee2a0797af18a1c6ee
kebabmaster <ieshevyakov@gmail.com>
2020-03-04 16:40:35 +0000

    <style> element generated on render - working on HTML and non-HTML tooltips

---
+++ src/utils/uuid.js
@@ -0,0 +1,17 @@
+/**
+* By w3resource, 2020, https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+*/
+
+export function generateUUID(){
+    var dt = new Date().getTime();
+
+    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c === "x" ? r :(r&0x3|0x8)).toString(16);
+    });
+
+    uuid = "t" + uuid.substring(1, uuid.length); // CSS does not work correctly with classes starting with a numeric character
+
+    return uuid;
+}
