39acabbeafe68c54dd08c9191a171526a004c24e
Daniel Barion <danielbarionn@gmail.com>
2022-10-13 15:43:32 -0300

    Updates

--- node_modules/time-stamp/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare function timestamp(pattern?: string | Date, date?: Date): string
-declare function timestampUTC(pattern?: string | Date, date?: Date): string
-
-export default timestamp
-export { timestampUTC as utc };
