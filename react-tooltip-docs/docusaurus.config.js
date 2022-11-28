0fe925855086c7af451339ac011113811dd09de3
Daniel Barion <danielbarionn@gmail.com>
2022-11-28 19:52:36 -0300

    docs: add delays docs into the project

--- react-tooltip-docs/docusaurus.config.js
+++ react-tooltip-docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'React Tooltip',
+  tagline: 'Awesome React Tooltip component',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
