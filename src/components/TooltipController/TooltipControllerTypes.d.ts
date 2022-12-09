45c4fb8e722db83a9b2b66bb05201fef6fa455c8
gabrieljablonski <contact@gabrieljablonski.com>
2022-12-09 14:46:55 -0300

    extend `HTMLElement` with data attributes

--- src/components/TooltipController/TooltipControllerTypes.d.ts
+++ src/components/TooltipController/TooltipControllerTypes.d.ts
@@ -29,3 +29,18 @@ export interface ITooltipController {
   isOpen?: boolean
   setIsOpen?: (value: boolean) => void
 }
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    'data-tooltip-place'?: PlacesType
+    'data-tooltip-content'?: string | number
+    'data-tooltip-html'?: string
+    'data-tooltip-variant'?: VariantType
+    'data-tooltip-offset'?: number
+    'data-tooltip-wrapper'?: WrapperType
+    'data-tooltip-events'?: EventsType[]
+    'data-tooltip-position-strategy'?: PositionStrategy
+    'data-tooltip-delay-show'?: number
+    'data-tooltip-delay-hide'?: number
+  }
+}
