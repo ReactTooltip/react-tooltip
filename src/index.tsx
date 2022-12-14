d9434c55afa7d880d259d0e24397fe574663e963
gabrieljablonski <contact@gabrieljablonski.com>
2022-12-14 10:16:37 -0300

    feat: export component types

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,26 @@
 import './tokens.css'
+import type {
+  ChildrenType,
+  DataAttribute,
+  EventsType,
+  PlacesType,
+  PositionStrategy,
+  VariantType,
+  WrapperType,
+} from './components/Tooltip/TooltipTypes'
+import type { ITooltipController } from './components/TooltipController/TooltipControllerTypes'
+import type { ITooltipWrapper } from './components/TooltipProvider/TooltipProviderTypes'
 
 export { TooltipController as Tooltip } from './components/TooltipController'
 export { TooltipProvider, TooltipWrapper } from './components/TooltipProvider'
+export type {
+  ChildrenType,
+  DataAttribute,
+  EventsType,
+  PlacesType,
+  PositionStrategy,
+  VariantType,
+  WrapperType,
+  ITooltipController as ITooltip,
+  ITooltipWrapper,
+}
