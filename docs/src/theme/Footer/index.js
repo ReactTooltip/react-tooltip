f6711139633e58678bfc7e2bf1e834f5ff931f01
Daniel Barion <danielbarionn@gmail.com>
2023-01-02 15:03:48 -0300

    refactor: update anchor ads implementation on docs

--- docs/src/theme/Footer/index.js
+++ docs/src/theme/Footer/index.js
@@ -4,11 +4,8 @@ import Footer from '@theme-original/Footer'
 // import AdsContainerElement from '@site/src/components/AdsContainerElement'
 
 export default function FooterWrapper(props) {
-  let shadoWindow = null
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      shadoWindow = window
       const scriptElement = document.createElement('script')
       scriptElement.src =
         'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7420210265158247'
@@ -21,27 +18,19 @@ export default function FooterWrapper(props) {
         }
       }
       document.body.appendChild(scriptElement)
+
+      const adsElement = document.createElement('ins')
+      adsElement.classList.add('adsbygoogle')
+      adsElement.classList.add('adsanchor')
+      adsElement.setAttribute('ad-client', 'ca-pub-7420210265158247')
+      adsElement.setAttribute('ad-slot', '8159529807')
+      adsElement.setAttribute('full-width-responsive', 'true')
+      document.body.appendChild(adsElement)
     }
   }, [])
 
   return (
     <>
-      {shadoWindow && shadoWindow.document.body.clientWidth <= 768 && (
-        <ins
-          className="adsbygoogle"
-          style={{
-            bottom: 0,
-            display: 'block',
-            position: 'fixed',
-            width: '100%',
-            maxHeight: '100px',
-            height: '75px',
-          }}
-          data-ad-client="ca-pub-7420210265158247"
-          data-ad-slot="8159529807"
-          data-full-width-responsive="true"
-        />
-      )}
       <Footer {...props} />
       {/* <AdsContainerElement id="mobile-anchor-ads" /> */}
     </>
