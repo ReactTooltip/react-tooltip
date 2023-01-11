e8c3388d71873affae8f19cb19d087e08dc8fe80
Daniel Barion <danielbarionn@gmail.com>
2023-01-11 09:58:40 -0300

    refactor: remove mobile anchor ads implementation

--- docs/src/theme/Footer/index.js
+++ docs/src/theme/Footer/index.js
@@ -1,31 +1,9 @@
 /* eslint-disable import/no-unresolved */
-import React, { useEffect } from 'react'
+import React from 'react'
 import Footer from '@theme-original/Footer'
 // import AdsContainerElement from '@site/src/components/AdsContainerElement'
 
 export default function FooterWrapper(props) {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const scriptElement = document.createElement('script')
-      scriptElement.src =
-        'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7420210265158247'
-      scriptElement.async = true
-      scriptElement.onload = () => {
-        if (typeof window !== 'undefined') {
-          window.onload = () => {
-            if (window.document.body.clientWidth <= 768) {
-              ;(window.adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: 'ca-pub-7420210265158247',
-                enable_page_level_ads: true,
-                overlays: { bottom: true },
-              })
-            }
-          }
-        }
-      }
-    }
-  }, [])
-
   return (
     <>
       <Footer {...props} />
