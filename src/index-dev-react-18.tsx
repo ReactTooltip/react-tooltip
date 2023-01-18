81508bd98a9e525f8525b4f704b482fb7a2b6a60
Daniel Barion <danielbarionn@gmail.com>
2023-01-18 16:14:37 -0300

    chore: update minimum supported react version in package.json

---
+++ src/index-dev-react-18.tsx
@@ -0,0 +1,17 @@
+import { StrictMode, version } from 'react'
+// eslint-disable-next-line import/no-unresolved
+import { createRoot } from 'react-dom/client' // this is we are now using react < 18
+import './tokens.css'
+import App from './App'
+
+// eslint-disable-next-line no-console
+console.log('Parent folder loaded react version: ', version)
+
+const container = document.getElementById('app')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- src/index-dev.tsx
+++ src/index-dev.tsx
@@ -1,16 +1,8 @@
-import { StrictMode, version } from 'react'
-import { createRoot } from 'react-dom/client'
-import './tokens.css'
+import React, { version } from 'react'
+import ReactDOM from 'react-dom'
 import App from './App'
+import './tokens.css'
 
-// eslint-disable-next-line no-console
 console.log('Parent folder loaded react version: ', version)
 
-const container = document.getElementById('app')
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!)
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById('app'))
