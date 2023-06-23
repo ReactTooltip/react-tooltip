262911c634f4bea12bd6006509c443d84c403149
colynfulcrum <122113864+colynfulcrum@users.noreply.github.com>
2023-06-23 17:45:37 -0400

    Update compute-positions-types.d.ts
    Updated types on place

--- src/utils/compute-positions-types.d.ts
+++ src/utils/compute-positions-types.d.ts
@@ -1,11 +1,23 @@
 import type { Middleware } from '../components/Tooltip/TooltipTypes'
 
 export interface IComputePositions {
-  elementReference?: Element | HTMLElement | null
-  tooltipReference?: Element | HTMLElement | null
-  tooltipArrowReference?: Element | HTMLElement | null
-  place?: 'top' | 'right' | 'bottom' | 'left'
-  offset?: number
-  strategy?: 'absolute' | 'fixed'
-  middlewares?: Middleware[]
+  elementReference?: Element | HTMLElement | null;
+  tooltipReference?: Element | HTMLElement | null;
+  tooltipArrowReference?: Element | HTMLElement | null;
+  place?:
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'right'
+    | 'right-start'
+    | 'right-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'left'
+    | 'left-start'
+    | 'left-end';
+  offset?: number;
+  strategy?: 'absolute' | 'fixed';
+  middlewares?: Middleware[];
 }
