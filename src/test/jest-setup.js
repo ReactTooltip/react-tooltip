877fb33787ffa21db12bb49f38bdfc8d11dd2d5d
gabrieljablonski <contact@gabrieljablonski.com>
2023-08-02 16:44:16 -0300

    test: mock `ResizeObserver`

--- src/test/jest-setup.js
+++ src/test/jest-setup.js
@@ -2,3 +2,9 @@ import React from 'react'
 // whatever else you need in here
 
 global.React = React
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}))
