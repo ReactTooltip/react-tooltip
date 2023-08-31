551cfe47ac25fc83c3a8f3837a0f81aa394bb098
Daniel Barion <danielbarionn@gmail.com>
2023-08-31 11:32:54 -0300

    ads: show two ads on a client height higher or equal to 950px

--- docs/src/components/AdsContainerElement/index.tsx
+++ docs/src/components/AdsContainerElement/index.tsx
@@ -3,6 +3,7 @@ import './styles.css'
 
 const AdsContainerElement = () => {
   const containerRef = useRef<any>()
+  const containerRef2 = useRef<any>()
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -18,7 +19,26 @@ const AdsContainerElement = () => {
     }
   }, [])
 
-  return <div className="carbon-ads" ref={containerRef} />
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window?.document?.body?.clientHeight >= 950) {
+      const scriptElement = document.createElement('script')
+      scriptElement.src = '//cdn.carbonads.com/carbon.js?serve=CWYD553L&placement=react-tooltipcom'
+      scriptElement.id = '_carbonads_js'
+      scriptElement.async = true
+
+      if (containerRef2?.current) {
+        containerRef2.current.innerHTML = ''
+        containerRef2.current.appendChild(scriptElement)
+      }
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="carbon-ads" ref={containerRef} />
+      <div className="carbon-ads" ref={containerRef2} />
+    </>
+  )
 }
 
 export default AdsContainerElement
