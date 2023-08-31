cc91763dd71f42c9bd63e84a5e6342577b8ddcd3
Daniel Barion <danielbarionn@gmail.com>
2023-08-31 12:01:04 -0300

    ads: remove second ads container that was in testing

--- docs/src/components/AdsContainerElement/index.tsx
+++ docs/src/components/AdsContainerElement/index.tsx
@@ -3,7 +3,6 @@ import './styles.css'
 
 const AdsContainerElement = () => {
   const containerRef = useRef<any>()
-  const containerRef2 = useRef<any>()
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -19,26 +18,7 @@ const AdsContainerElement = () => {
     }
   }, [])
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window?.document?.body?.clientHeight >= 950) {
-      const scriptElement = document.createElement('script')
-      scriptElement.src = '//cdn.carbonads.com/carbon.js?serve=CWYD553L&placement=react-tooltipcom'
-      scriptElement.id = '_carbonads_js'
-      scriptElement.async = true
-
-      if (containerRef2?.current) {
-        containerRef2.current.innerHTML = ''
-        containerRef2.current.appendChild(scriptElement)
-      }
-    }
-  }, [])
-
-  return (
-    <>
-      <div className="carbon-ads" ref={containerRef} />
-      <div className="carbon-ads" ref={containerRef2} />
-    </>
-  )
+  return <div className="carbon-ads" ref={containerRef} />
 }
 
 export default AdsContainerElement
