d86555742e0027098b972e18fcc9334203901f0f
gabrieljablonski <contact@gabrieljablonski.com>
2023-09-05 19:39:29 -0300

    docs: frigade sponsor docs

--- docs/src/theme/TOC/index.js
+++ docs/src/theme/TOC/index.js
@@ -2,12 +2,29 @@
 import React from 'react'
 import TOC from '@theme-original/TOC'
 import AdsContainerElement from '@site/src/components/AdsContainerElement'
+import LogoFrigade from '@site/static/img/sponsors/frigade.png'
 
 export default function TOCWrapper(props) {
   return (
     <>
       <TOC {...props} />
-      <AdsContainerElement />
+      <div
+        style={{
+          position: 'fixed',
+        }}
+      >
+        <div className="sponsor-frigade">
+          <a
+            href="https://frigade.com/?source=react-tooltip"
+            title="Frigade"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={LogoFrigade} alt="Frigade" />
+          </a>
+        </div>
+        <AdsContainerElement />
+      </div>
       {/* <ins
         className="adsbygoogle"
         style={{ display: 'block', position: 'fixed', width: '300px', height: '600px' }}
