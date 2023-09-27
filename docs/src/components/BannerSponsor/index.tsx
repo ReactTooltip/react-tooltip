c01b475f0f1e83ec51ec0cd3ee63f6b2d77fbbf5
gabrieljablonski <contact@gabrieljablonski.com>
2023-09-27 17:30:00 -0300

    docs: docs dopt sponsor

---
+++ docs/src/components/BannerSponsor/index.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+// @ts-ignore
+import LogoFrigade from '@site/static/img/sponsors/frigade.png'
+import LogoDopt from '@site/static/img/sponsors/dopt.png'
+import './styles.css'
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    dataLayer?: any
+  }
+}
+
+const SPONSORS = {
+  frigade: {
+    logo: LogoFrigade,
+    title: 'Frigade',
+  },
+  dopt: {
+    logo: LogoDopt,
+    title: 'Dopt',
+  },
+}
+
+interface BannerSponsorProps {
+  sponsorKey: keyof typeof SPONSORS
+}
+
+const BannerSponsor = ({ sponsorKey }: BannerSponsorProps) => {
+  const sponsor = SPONSORS[sponsorKey]
+
+  const onClickSponsorBannerEventHandler = () => {
+    if (typeof window !== 'undefined') {
+      window.dataLayer = window.dataLayer || []
+
+      window.dataLayer.push({
+        event: `click_${sponsorKey}_banner`,
+        place: 'sidebar',
+      })
+    }
+
+    return true
+  }
+
+  return (
+    <div className="sponsor-banner">
+      <a
+        href="https://frigade.com/?source=react-tooltip"
+        title={sponsor.title}
+        target="_blank"
+        rel="noreferrer"
+        onClick={onClickSponsorBannerEventHandler}
+      >
+        <img src={sponsor.logo} alt={sponsor.title} />
+      </a>
+    </div>
+  )
+}
+
+export default BannerSponsor
