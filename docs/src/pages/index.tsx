d20053d7026294b873ae8369f738e6b9634b36d2
gabrieljablonski <gabriel.g.jablonski@gmail.com>
2023-10-17 18:01:31 -0300

    docs: fix home header alignment

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className={clsx('container', styles.heroContainer)}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__logo">
           <img src="img/logo.svg" alt="Happy face with a tooltip saying 'Hello I'm a tooltip'" />
