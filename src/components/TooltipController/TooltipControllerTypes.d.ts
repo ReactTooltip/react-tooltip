83b9d219d879bc1cb3d90a7dd2fe32a5803da566
gabrieljablonski <contact@gabrieljablonski.com>
2023-10-23 11:25:54 -0300

    revert: `holdPositionOnRender`

--- src/components/TooltipController/TooltipControllerTypes.d.ts
+++ src/components/TooltipController/TooltipControllerTypes.d.ts
@@ -61,7 +61,6 @@ export interface ITooltipController {
   position?: IPosition
   isOpen?: boolean
   disableStyleInjection?: boolean | 'core'
-  holdPositionOnRender?: boolean
   /**
    * @description see https://developer.mozilla.org/en-US/docs/Web/CSS/border.
    *
