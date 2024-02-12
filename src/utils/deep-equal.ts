1f6f2c13effbc92d6c1cc697a68476685a1ec5ba
gabrieljablonski <contact@gabrieljablonski.com>
2024-02-12 11:23:04 -0300

    feat: improved `deepEqual()`

--- src/utils/deep-equal.ts
+++ src/utils/deep-equal.ts
@@ -1,27 +1,34 @@
 const isObject = (object: unknown): object is Record<string, unknown> => {
-  return object !== null && typeof object === 'object'
+  return object !== null && !Array.isArray(object) && typeof object === 'object'
 }
 
 const deepEqual = (object1: unknown, object2: unknown): boolean => {
+  if (object1 === object2) {
+    return true
+  }
+
+  if (Array.isArray(object1) && Array.isArray(object2)) {
+    if (object1.length !== object2.length) {
+      return false
+    }
+    return object1.every((val, index) => deepEqual(val, object2[index]))
+  }
+
+  if (Array.isArray(object1) !== Array.isArray(object2)) {
+    return false
+  }
+
   if (!isObject(object1) || !isObject(object2)) {
     return object1 === object2
   }
 
   const keys1 = Object.keys(object1)
   const keys2 = Object.keys(object2)
-
   if (keys1.length !== keys2.length) {
     return false
   }
 
-  return keys1.every((key) => {
-    const val1 = object1[key]
-    const val2 = object2[key]
-    if (isObject(val1) && isObject(val2)) {
-      return deepEqual(val1, val2)
-    }
-    return val1 === val2
-  })
+  return keys1.every((key) => deepEqual(object1[key], object2[key]))
 }
 
 export default deepEqual
